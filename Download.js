d47aefc8746055b4b9dd8c5d8b7aaaff65c02225
wallpaperave <30646206+wallpaperave@users.noreply.github.com>
2017-08-02 12:23:19 +0700

    Update Download.js

--- Download.js
+++ Download.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('img.downloadable').each(function(){
         var $this = $(this);
-        $this.wrap('<a href="' + $this.attr('src') + '" download />')
+        $this.wrap('<a download />')
     });
 });
